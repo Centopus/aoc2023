18732d5b16c07707ba8f73fca4d084c0027d04fa
Centopus <github@arrow.ovh>
2023-12-02 15:20:28 +0000

    Day 1 done

--- Day01/parseInput.v2.py
+++ Day01/parseInput.v2.py
@@ -6,9 +6,10 @@
 #
 import re
 
-#input = open('inputFile','r')
-input = open('miniPut','r')
+input = open('inputFile','r')
+#input = open('miniPut','r')
 counter = 0
+output = 0
 
 #patterns need to be compiled before can be used
 #single digit
@@ -22,8 +23,18 @@ while True:
     if not line:
         break
     
-    out = p1.match(line)
-    print(line)
-    print(out)
+    out = p1.findall(line)
+    #print(line)
+    #we need first digit
+    #print(out[0])
+    #we need last digit
+    #print(out[len(out)-1])
+    #we need concatenate those
+    tuple = out[0] + out[len(out)-1]
+    #print(tuple)
+    
+    output += int(tuple)
+    print(output)
+    
 
 input.close()
