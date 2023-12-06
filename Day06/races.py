3a515794371886d75ec34b40e68a2a3b1d4e3f4b
Centopus <github@arrow.ovh>
2023-12-06 08:08:34 +0000

    part1

--- Day06/races.py
+++ Day06/races.py
@@ -12,7 +12,7 @@ def readInput(filename):
 
 #data
 data = readInput('inputMini')
-#data = readInput('inputFile')
+data = readInput('inputFile')
 
 def numbers(line):
     numbers = line.split(' ')
@@ -25,8 +25,44 @@ def parsing(line):
     data = numbers(out[1])
     return data
 
+def raceInc(Zeit,Strecke):
+    time=int(Zeit)
+    dist=int(Strecke)
+    for chargetime in range(1,time,1):
+        speed=chargetime;
+        distance=speed*(time-chargetime);
+        if distance > dist:
+            #you win
+            return chargetime
+
+def raceDesc(Zeit,Strecke):
+    time=int(Zeit)
+    dist=int(Strecke)
+    for chargetime in range(time,1,-1):
+        speed=chargetime;
+        distance=speed*(time-chargetime);
+        if distance > dist:
+            #you win
+            return chargetime
+
 #main
+win=[]
 Time=parsing(data[0])
 Distance=parsing(data[1])
-
 print(Time,Distance)
+i=0
+while i <= (len(data)+1):
+    win.append([raceInc(Time[i],Distance[i]),raceDesc(Time[i],Distance[i])])
+    i +=1
+print(win)
+output=1
+for a in win:
+    wins=0
+    for b in range(a[0],a[1]+1,1):
+        wins+=1
+        #print(b)
+    print(wins)
+    output=output*wins
+    #print(output)
+print(output)
+
