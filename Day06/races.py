bb2eacc7c196f74be19b30d46212ab158b3cd86c
Centopus <github@arrow.ovh>
2023-12-06 06:12:11 +0000

    data read, now need to use it

---
+++ Day06/races.py
@@ -0,0 +1,32 @@
+#
+# day6 advent of code
+# by Centopus
+# in python
+# 2023.12.06
+#
+
+def readInput(filename):
+    with open(filename) as var1:
+        data = [line.rstrip() for line in var1.readlines()]
+    return data
+
+#data
+data = readInput('inputMini')
+#data = readInput('inputFile')
+
+def numbers(line):
+    numbers = line.split(' ')
+    numbers = [i for i in numbers if i != '']
+    return numbers
+
+def parsing(line):
+    out = line.split(':')
+    data_tag = (out[0])
+    data = numbers(out[1])
+    return data
+
+#main
+Time=parsing(data[0])
+Distance=parsing(data[1])
+
+print(Time,Distance)
